c3c280ebe30f4904d2e3783f152cd0047816ea85
muratavci05 <js.muratavci@gmail.com>
2022-12-06 17:10:17 +0300

    nasaData component in get api css modified

--- src/components/nasaData.js
+++ src/components/nasaData.js
@@ -1,43 +1,46 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
+const NasaData = () => {
+  const [photoData, setPhotoData] = useState(null);
 
-const NasaData = () =>{
-
-const [photoData, setPhotoData]=useState(null);
-
-
-useEffect(()=>{
+  useEffect(() => {
     fetchPhoto();
-    async function fetchPhoto(){
-        const res = await fetch(
-            `https://api.nasa.gov/planetary/apod?api_key=Djvk0izcCaS7jkIWmRyhXx01lm7fLOvXnWdVoG5O`
-            
-        );
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=Djvk0izcCaS7jkIWmRyhXx01lm7fLOvXnWdVoG5O`
+      );
 
-        
-        const data = await res.json();
-        setPhotoData(data);
-        console.log(data)
+      const data = await res.json();
+      setPhotoData(data);
+      console.log(data);
     }
-},[]);
+  }, []);
 
-if (!photoData) return <div/>;
+  if (!photoData) return <div />;
 
-
-
-    return(
-        <React.Fragment>
-            <div>
-                <img src={photoData.url} 
-                alt={photoData.data}
-                />
-            </div>
-            <h1>{photoData.title}</h1>
-            <h3>{photoData.date}</h3>
-            <p>{photoData.explanation}</p>
-            <h6>{photoData.hdurl}</h6>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div style={{ display: "flex" }}>
+        <div>
+          <img
+            style={{
+              border: "1px solid black",
+              width: "600px",
+              height: "auto",
+            }}
+            src={photoData.url}
+            alt={photoData.data}
+          />
+        </div>
+        <div style={{ border: "1px solid blue" }}>
+          <h1>{photoData.title}</h1>
+          <h3>{photoData.date}</h3>
+          <p>{photoData.explanation}</p>
+          <h6>{photoData.hdurl}</h6>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default NasaData;
