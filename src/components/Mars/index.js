44243eb167aac892cfea95127a52c9c04e3dc32b
muratavci05 <js.muratavci@gmail.com>
2022-12-11 01:45:13 +0300

    mars component modified and get api

--- src/components/Mars/index.js
+++ src/components/Mars/index.js
@@ -1,44 +1,53 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Image } from "antd";
 
-const Mars = () => {
-const[marsData,setMarsData]=useState([])
+const Mars = (props) => {
+  const [apod, setApod] = useState([]);
 
-    useEffect(()=>{
-        axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
-          .then((res)=>{
-            console.log("MARS-API-GET-RESPONSE >>:",res.data)
-            marsData(res.data)
-          })
-          .catch((err)=>{
-            console.log("MARS-API-GET-ERROR >>:",err)
-          })
-    
-      },[])
-
-      const arr = data.map((data, index)=>{
-        return(
-            
-        )
+  useEffect(() => {
+    const count = 20;
+    const apiKey = "QEqJuF17YEpNksxMBoIn16iJS67hKVicRUrfEq0m";
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`
+      )
+      .then((res) => {
+        console.log("MarsAPP Res>>", res.data);
+        setApod(res.data);
       })
-      
+      .catch((err) => {
+        console.log("hata mesajı", err);
+      });
+  }, []);
 
-    return(
-        <React.Fragment>
-            <div>
-                <h3>burası mars verileri barındırır</h3>
-                <div>
-                    <h3>name :</h3>
-                    <h3>id :</h3>
-                    <h3>earth_date :</h3>
-                    <h3>img_src :</h3>
-                    <h3>rover_name :  </h3>
-
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row row-cols-6 row-cols-sm-2 row-cols-md-1 g-3">
+          {apod.map((item, index) => {
+            return (
+              <div key={index} className="col">
+                <div className="card shadow-sm d-flex flex-row">
+                  <div className="Image">
+                    <Image width={150} height={150} src={item.hdurl} />
+                  </div>
+                  <div className="MarsText">
+                    <h4 className="copyright">{item.copyright}</h4>
+                    <h4 className="title">
+                      <span>*</span>
+                      {item.title}
+                    </h4>
+                    <p className="explanation">{item.explanation}</p>
+                  </div>
                 </div>
-
-            </div>
-        </React.Fragment>
-    )
-}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Mars;
