dfdf8fc5700a74f0b11c973e4ea40f133e176e90
muratavci05 <js.muratavci@gmail.com>
2022-12-22 23:18:41 +0300

    footer modified

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import GithubLogo from "../assets/shared/githubLogo.svg";
 import LinkedinLogo from "../assets/shared/linkedinLogo.svg";
-import NasaDayPhoto from "../assets/shared/nasa-logo.svg";
+import SpaceLogo from "../assets/shared/log.svg";
 
 const Footer =()=>{
     return(
@@ -15,9 +15,9 @@ const Footer =()=>{
                         </div>
                         <p style={{color:"grey",display:"flex",justifyContent:"center"}}>Murat AVCI <span style={{color:"white"}}>&nbsp;<span> | </span>&nbsp;   </span> <p style={{color:"gray"}}>React Frontend Developer</p></p>
                        <div style={{display:"flex", justifyContent:"center"}}>
-                        <a href="https://github.com/muratavci05"><img src={GithubLogo} style={{backgroundColor:"white",borderRadius:"50%",width:"25px"}}/></a> &nbsp;
-                        <a href="https://www.linkedin.com/in/muratavci05/"><img src={LinkedinLogo} style={{backgroundColor:"white",borderRadius:"50%",width:"25px"}}/></a>
-                        <a href="/nasa-photo"><img src={NasaDayPhoto} style={{borderRadius:"50%",width:"45px"}}/></a>
+                        <a href="https://github.com/muratavci05"><img src={GithubLogo} style={{backgroundColor:"white",borderRadius:"50%",width:"25px"}}/></a> &nbsp;&nbsp;
+                        <a href="https://www.linkedin.com/in/muratavci05/"><img src={LinkedinLogo} style={{backgroundColor:"white",borderRadius:"50%",width:"25px"}}/></a>&nbsp;&nbsp;
+                        <a href="/nasa-photo"><img src={SpaceLogo} style={{borderRadius:"50%",width:"25px"}}/></a>
 
                         </div>
                     </div>
